52df8acd34a059278fd310511e38cc32348904a4
Will Darkins <willdarkins@gmail.com>
2021-10-31 14:23:02 -0700

    Misprinted DataTypes causing an error when running server -- this has been fixed

--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,7 @@ class Category extends Model {}
 Category.init(
   {
     id: {
-      type: Datatypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
