76d696b920972e0e36fdae246fff8ff252f40773
Will Darkins <willdarkins@gmail.com>
2021-10-31 12:45:05 -0700

    foreign keys set in models index page

--- models/index.js
+++ models/index.js
@@ -6,21 +6,21 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'user_id'
+  foreignKey: 'id'
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'user_id'
+  foreignKey: 'id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongToMany(Tag, {
   through: ProductTag,
-  foreignKey: ''
+  foreignKey: 'id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongToMany(Product, {
   through: ProductTag,
-  foreignKey: ''
+  foreignKey: 'id'
 })
 
 module.exports = {
