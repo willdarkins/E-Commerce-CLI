c2899c4fb0b399202ad387846e9441440946a1a8
Will Darkins <willdarkins@gmail.com>
2021-10-31 13:39:32 -0700

    Went back to models index to specify which ids were referenced by foreignkeys

--- models/index.js
+++ models/index.js
@@ -6,21 +6,29 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'id'
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongToMany(Product, {
   through: ProductTag,
-  foreignKey: 'id'
+  foreignKey: 'tag_id'
+})
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+})
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
 })
 
 module.exports = {
