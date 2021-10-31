e558475d3c492ad6c768b37c12d7b908b61ba1c4
Will Darkins <willdarkins@gmail.com>
2021-10-31 14:10:29 -0700

    wrote code for findAll categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,12 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  Category.findAll({})
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Products
 });
 
