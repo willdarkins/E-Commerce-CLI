8530aea48d959786b45a7e6a014014fc3fa04425
Will Darkins <willdarkins@gmail.com>
2021-10-31 16:39:55 -0700

    having issues connecting to the database in order seed data... asking VLS

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require('sequelize');
 
 class Category extends Model {}
 
