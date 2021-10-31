20c6685caf5c03fd5216e19b57210e81c344d047
Will Darkins <willdarkins@gmail.com>
2021-10-31 12:39:02 -0700

    rough sketch of model index files is laid out, but still working with foreignkey relations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'user_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'user_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreignKey: ''
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  foreignKey: ''
+})
 
 module.exports = {
   Product,
