608d1a0261a8fa386312a4cdf5eb251a77648883
Will Darkins <willdarkins@gmail.com>
2021-10-31 23:34:15 -0700

    POST route which updates a category name based on id has been set and tested

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,7 +54,21 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
